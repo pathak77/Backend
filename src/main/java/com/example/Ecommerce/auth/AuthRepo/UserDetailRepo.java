0b1485e2432d67dd26efbf561edbde1df07d473e
Aayush Pathak <aayushpathak77@gmail.com>
2025-07-14 20:52:46 +0545

    chenged somethings

---
+++ src/main/java/com/example/Ecommerce/auth/AuthRepo/UserDetailRepo.java
@@ -0,0 +1,16 @@
+package com.example.Ecommerce.auth.AuthRepo;
+
+import com.example.Ecommerce.auth.AuthticationEntities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+
+@Repository
+public interface UserDetailRepo extends JpaRepository<User, UUID> {
+
+    public User findByUsername(String username);
+
+    User findByEmail(String username);
+}
